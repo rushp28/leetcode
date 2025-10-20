cea948fcb3f727402cfcc0d3b2f68bc7b42ade12
rushp28 <CB011487@students.apiit.lk>
2025-10-20 11:41:56 +0530

    [feat] solve 1 JSON-related problem - 2677: split a given array of elements into chunks of a given size.

--- 30_days_of_javascript/json.js
+++ 30_days_of_javascript/json.js
@@ -7,3 +7,19 @@
 const isEmpty = (obj) => {
   return Object.keys(obj).length === 0;
 };
+
+/**
+ * 2677
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size){
+  let array_chunks = [];
+
+  while (arr.length > 0) {
+    array_chunks.push(arr.splice(0, size));
+  }
+
+  return array_chunks;
+};
