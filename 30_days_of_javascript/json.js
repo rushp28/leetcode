c1918737acc76b7639f00edc841c48243e54140a
rushp28 <CB011487@students.apiit.lk>
2025-10-20 13:57:42 +0530

    [feat] solve 1 JSON-related problem - 2619: return the last element of an array or -1 if the array is empty using prototype.last().

--- 30_days_of_javascript/json.js
+++ 30_days_of_javascript/json.js
@@ -21,3 +21,11 @@ var chunk = function (arr, size) {
   }
   return arr_chunks;
 };
+
+/**
+ * 2619
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+  return this.length ? this[this.length - 1] : -1;
+};
